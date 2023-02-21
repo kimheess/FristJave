0d9bc30de53bb752ddc9805c9bfc43ebed15b9e1
heesu <cucucu1232@naver.com>
2023-02-21 14:34:44 +0900

    commit tt

--- JavaGit/src/tset.java
+++ JavaGit/src/tset.java
@@ -1,1 +1,13 @@
-pull 해보기
+
+
+public class tset {
+
+	public static void main(String[] args) {
+		System.out.println("pull 해보기");
+		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 완료");
+
+		
+	}
+
+}
