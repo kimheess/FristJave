a51a6f0229dda63a17c707f0f74a5192a992500e
heesu <cucucu1232@naver.com>
2023-02-21 13:58:10 +0900

    dSZasd

--- JavaGit/src/Git.java
+++ JavaGit/src/Git.java
@@ -8,6 +8,9 @@ public class Git {
 		System.out.println("코드 작성 중");
 		System.out.println("코드 작성 중");
 		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 중");
 		
 	}
 
