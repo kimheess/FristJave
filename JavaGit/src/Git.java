f56312fcd18df2ea0b2aeed06f23ef5db884321f
heesu <cucucu1232@naver.com>
2023-02-21 14:18:14 +0900

    commit test

--- JavaGit/src/Git.java
+++ JavaGit/src/Git.java
@@ -4,14 +4,8 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
-		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 완료");
+
 		
 	}
 
