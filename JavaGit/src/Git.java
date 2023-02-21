10e2817f87684019f96b192b48cb566f8b568d21
heesu <cucucu1232@naver.com>
2023-02-21 13:56:08 +0900

    FirstJave

--- JavaGit/src/Git.java
+++ JavaGit/src/Git.java
@@ -4,6 +4,11 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 중");
+		System.out.println("코드 작성 중");
+		
 	}
 
 }
